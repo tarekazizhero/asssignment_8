c515fac360a1d0b1d0f235be1b776b881a48a78e
tarek aziz <1720114.tarekaziz@gmail.com>
2023-11-24 08:17:32 +0600

    eight  assignment

---
+++ asssignment_8/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donal Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        return response($data, 200)->cookie(
+            $cookieName,
+            $cookieValue,
+            $minutes,
+            $path,
+            $domain,
+            $secure,
+            $httpOnly
+        );
+    }
+}
